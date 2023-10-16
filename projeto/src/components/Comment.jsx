1776bc43bfacbdd68c4261894a957fcba595f411
Jhonanttan <jhonanttan1@gmail.com>
2023-10-16 14:32:38 -0300

    Finalizando design antes de aplicar a responsividade

---
+++ projeto/src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://avatars.githubusercontent.com/u/108599288?v=4" alt="" />
+            
+            <div className={styles.commentBox}> 
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Jhon Willyan</strong>
+                            <time title='15 de Outubro às 11:51' dateTime='2023-15-10 11:51:30'>Cerca de 1h atrás</time>
+
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>
+                        Muito bom, zé da Manga!!
+                    </p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
