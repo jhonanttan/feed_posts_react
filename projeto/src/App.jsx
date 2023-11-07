3dd500555f6c1c994807f873b48c7824bede0a4c
Jhonanttan <jhonanttan1@gmail.com>
2023-11-07 10:14:22 -0300

    Aplicando comentários dinamicos com o usestates do react

--- projeto/src/App.jsx
+++ projeto/src/App.jsx
@@ -5,6 +5,43 @@ import { Sidebar } from './components/Sidebar';
 import './global.css';
 import styles from './App.module.css';
 
+
+// author: { avatar_url: "", name: "", role: ""}
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'http://github.com/jhonanttan.png',
+      name: 'Jhon',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋',},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: '<a href="#">jane.design/doctorcare</a>' },
+    ],
+    publishedAt: new Date('2023-10-16 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'http://github.com/jhonanttan.png',
+      name: 'Zé da manga',
+      role: 'Web Developer'
+    },
+    content: [
+    { type: 'paragraph', content: 'Fala galeraa 👋',},
+    { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+    { type: 'link', content: '<a href="#">jane.design/doctorcare</a>' }
+    ],
+    publishedAt: new Date('2023-10-17 20:00:00'),
+  },
+];
+
+
 export function App() {
   return (
     <div>
@@ -13,10 +50,15 @@ export function App() {
     <div className={styles.wrapper}>
       <Sidebar />
       <main>
-    <Post 
-      author="Jhon GOSTOSO" 
-      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quibusdam repellat expedita autem nisi veniam vero, obcaecati eveniet dolore aliquam iste eius fugit velit dolorum magnam. Ullam sapiente illo sequi!"
-    />
+        {posts.map(post => {
+          return (
+            <Post 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ) 
+        })}
       </main>
     </div>
     
--- projeto/src/components/Post.jsx
+++ projeto/src/components/Post.jsx
@@ -1,44 +1,77 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
+import { useState } from 'react';
 
-export function Post() {
+
+export function Post({ author, publishedAt, content }) {
+    const [comments, setComments  ] = useState([
+       'post muito bacana, zé da manga!'
+    ])
+
+    const [newCommentText, setNewCommentText] = useState('')
+    
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    });
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+
+    })
+
+    function handleCreateNewComment(event) {
+        event.preventDefault();
+        setComments([...comments, newCommentText])
+        setNewCommentText('')
+    }
+
+    function handleNewCommentChange() {
+        setNewCommentText(event.target.value);
+    }
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://avatars.githubusercontent.com/u/108599288?v=4" alt="" />
+                    <Avatar src={author.avatarUrl} alt="" />
                     <div className={styles.authorInfo}>
-                        <strong>Jhonanttan Willyan</strong>
-                        <span>Web developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
 
-                <time title='15 de Outubro às 11:51' dateTime='2023-15-10 11:51:30'>Públicado há 1h</time>
+                <time title='15 de Outubro às 11:51' dateTime={publishedAt.toISOString}>
+                    {publishedDateRelativeToNow}
+                </time>
+                
             </header>
 
             <div className={styles.content}>
-            <p>Fala galeraa 👋</p>
-
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-            <p><a href="#">jane.design/doctorcare</a></p>
-
-            <p>
-                <a href="#">#novoprojeto </a> {' '}
-                <a href="#">#nlw </a> {' '}
-                <a href="#">#rocketseat</a> {' '}
-            </p>
+            {content.map(line => {
+                if (line.type === 'paragraph') {
+                    return <p>{line.content}</p>;
+                } else if (line.type === 'link') {
+                    return <p> <a href="">{line.content}</a> </p>;
+                }
+            })}
 
             </div>
 
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea 
+                    name='comment'
                     placeholder='Deixe um comentário'
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
                 />
 
                 <footer>
@@ -47,9 +80,9 @@ export function Post() {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(commment => {
+                    return <Comment content={commment} />
+                })}
             </div>
 
         </article>
